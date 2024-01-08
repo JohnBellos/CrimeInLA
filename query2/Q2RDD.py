f42fee23bcfecf1bfb3e7240518234ff2a1ebec5
master <master@example.com>
2024-01-08 22:05:43 +0200

    query2 rdd

--- query2/Q2RDD.py
+++ query2/Q2RDD.py
@@ -1,51 +1,64 @@
-from pyspark.sql import SparkSession
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SQLContext
 
-sc = SparkSession \
-    .builder \
-    .appName("RDD query 2 execution") \
-    .getOrCreate() \
-    .sparkContext
+# Create a Spark configuration and set the app name
+conf = SparkConf().setAppName("RDD query 2 execution")
+sc = SparkContext(conf=conf)
 
-def get_period(hour):
-    if 500 <= hour < 1200:
+# Create a SQLContext to work with Spark SQL
+sqlContext = SQLContext(sc)
+
+# Specify the path to your CSV file
+csv_file_path_1 = "hdfs://okeanos-master:54310/data/crime-data-from-2010-to-2019.csv"
+csv_file_path_2 = "hdfs://okeanos-master:54310/data/crime-data-from-2020-to-present.csv"
+
+# Use the Spark SQL context to read the CSV file into a DataFrame
+rdd1 = sqlContext.read.format("com.databricks.spark.csv") \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .load(csv_file_path_1) \
+    .rdd
+
+rdd2 = sqlContext.read.format("com.databricks.spark.csv") \
+    .option("header", "true") \
+    .option("inferSchema", "true") \
+    .load(csv_file_path_2) \
+    .rdd
+
+rdd = rdd1.union(rdd2)
+
+rdd = rdd.filter(lambda row: row[15] == 'STREET')
+# Show the DataFrame
+# df.show()
+print(rdd.take(3))
+
+# Define a function to categorize the time into periods
+def categorize_time(row):
+    time_occ = int(row[3])
+    if 500 <= time_occ < 1200:
         return "Morning"
-    elif 1200 <= hour < 1700:
+    elif 1200 <= time_occ < 1700:
         return "Afternoon"
-    elif 1700 <= hour < 2100:
+    elif 1700 <= time_occ < 2100:
         return "Evening"
     else:
         return "Night"
 
-crimes1 = sc.textFile("hdfs://okeanos-master:54310/data/crime-data-from-2010-to-2019.csv").rdd
-print(crimes1.take(2))
-crimes2 = sc.textFile("hdfs://okeanos-master:54310/data/crime-data-from-2020-to-present.csv").map(lambda x: (x.split(",")))
+# Use map to categorize the time and create key-value pairs
+categorized_rdd = rdd.map(lambda row: (categorize_time(row), 1))
 
-header = crimes1.first()
-print(header)
+# Use reduceByKey to count occurrences for each period
+count_by_period = categorized_rdd.reduceByKey(lambda a, b: a + b)
 
-crimes1 = crimes1.filter(lambda line: line != header).collect()
-print(crimes1.take(2))
+# Use sortBy to rank periods from most to least occurrences
+sorted_periods = count_by_period.sortBy(lambda x: x[1], ascending=False)
 
-header = crimes2.first()
-print(header)
-crimes2 = crimes2.filter(lambda line: line != header).collect()
-print(crimes2.take(2))
+# Collect the results
+result = sorted_periods.collect()
 
-crimes = crimes1.union(crimes2)
-crimes = crimes.filter(lambda line: line != header)
-print(crimes.take(2))
+# Display the result
+for period, count in result:
+    print(f"{period}: {count} occurrences")
+# Stop the SparkContext
+sc.stop()
 
-
-# Apply the mapping function to the RDD
-mapped_rdd = crimes1.map(lambda x: get_period(int(x[3])))
-
-# Count the occurrences of each period
-count_rdd = mapped_rdd.map(lambda x: (x[0], 1)).reduceByKey(lambda a, b: a + b)
-
-# Sort the result in descending order based on the count
-sorted_rdd = count_rdd.sortBy(lambda x: x[0], ascending=False)
-
-# Collect and print the result
-result = sorted_rdd.collect()
-for row in result:
-    print(row)
